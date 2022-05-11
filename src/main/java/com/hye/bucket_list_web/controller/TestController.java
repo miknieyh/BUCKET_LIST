93c7b338f8d6243e740c6136df7fe0fd1f149570
miknieyh <tong_hye@naver.com>
2022-05-11 16:49:54 +0900

    타임리프 셋팅

--- src/main/java/com/hye/bucket_list_web/controller/TestController.java
+++ src/main/java/com/hye/bucket_list_web/controller/TestController.java
@@ -1,13 +1,16 @@
 package com.hye.bucket_list_web.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class TestController {
 
-    @GetMapping
-    public String hello(){
-        return "index";
+    @RequestMapping(value = "/",method = RequestMethod.GET)
+    public String hello(Model model){
+        model.addAttribute("hello","테스트 메세지 입니다.");
+        return "/index";
     }
 }
